f9dcf447d27d4f782192b4e02e5d54203c4743a4
Noa Amit <noa1amit2@gmail.com>
2023-04-12 17:37:22 +0300

    strt job search screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import './screens/babysitter_profile_screen.dart';
 import './screens/login_screen.dart';
 import './screens/welcome_screen.dart';
 import './screens/filter_screen.dart';
+import './screens/jobs_search_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,14 +15,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       // initialRoute: WelcomeScreen.routeName,
-      initialRoute: BabysitterProfileScreen.routeName,
+      // initialRoute: BabysitterProfileScreen.routeName,
       // initialRoute: FilterScreen.routeName,
+      initialRoute: JobsSearchScreen.routeName,
       routes: {
         LoginScreen.routeName: (context) => LoginScreen(),
         BabysitterProfileScreen.routeName: (context) =>
             BabysitterProfileScreen(),
         WelcomeScreen.routeName: (context) => WelcomeScreen(),
         FilterScreen.routeName: (context) => FilterScreen(),
+        JobsSearchScreen.routeName: (context) => JobsSearchScreen(),
       },
     );
   }
