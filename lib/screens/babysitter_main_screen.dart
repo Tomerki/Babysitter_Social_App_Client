e6f770446a4538f6c183e30b913dae69f939a053
Noa Amit <noa1amit2@gmail.com>
2023-06-07 13:47:31 +0300

    jdjdb

--- lib/screens/babysitter_main_screen.dart
+++ lib/screens/babysitter_main_screen.dart
@@ -157,45 +157,47 @@ class _BabysitterMainScreenState extends State<BabysitterMainScreen> {
         // backgroundColor: Color.fromARGB(255, 219, 163, 154),
       ),
       drawer: MainDrawer(),
-      body: PersistentTabView(
-        context,
-        controller: _controller,
-        screens: _screens,
-        items: _navBarsItems(),
-        confineInSafeArea: true,
-        onItemSelected: (index) {
-          setState(() {
-            screen_name = _screenName[index];
-          });
-        },
+      body: SingleChildScrollView(
+        child: PersistentTabView(
+          context,
+          controller: _controller,
+          screens: _screens,
+          items: _navBarsItems(),
+          confineInSafeArea: true,
+          onItemSelected: (index) {
+            setState(() {
+              screen_name = _screenName[index];
+            });
+          },
 
-        backgroundColor: Color.fromARGB(255, 129, 100, 110)
-            .withOpacity(0.5), // Default is Colors.white.
-        handleAndroidBackButtonPress: true, // Default is true.
-        resizeToAvoidBottomInset:
-            true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
-        stateManagement: false, // Default is true.
-        hideNavigationBarWhenKeyboardShows:
-            true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
-        decoration: NavBarDecoration(
-          borderRadius: BorderRadius.circular(0.0),
-          colorBehindNavBar: Colors.white,
+          backgroundColor: Color.fromARGB(255, 129, 100, 110)
+              .withOpacity(0.5), // Default is Colors.white.
+          handleAndroidBackButtonPress: true, // Default is true.
+          resizeToAvoidBottomInset:
+              true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
+          stateManagement: false, // Default is true.
+          hideNavigationBarWhenKeyboardShows:
+              true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
+          decoration: NavBarDecoration(
+            borderRadius: BorderRadius.circular(0.0),
+            colorBehindNavBar: Colors.white,
+          ),
+          popAllScreensOnTapOfSelectedTab: true,
+          popActionScreens: PopActionScreensType.all,
+          itemAnimationProperties: const ItemAnimationProperties(
+            // Navigation Bar's items animation properties.
+            duration: Duration(milliseconds: 200),
+            curve: Curves.ease,
+          ),
+          screenTransitionAnimation: const ScreenTransitionAnimation(
+            // Screen transition animation on change of selected tab.
+            animateTabTransition: true,
+            curve: Curves.ease,
+            duration: Duration(milliseconds: 200),
+          ),
+          navBarStyle: NavBarStyle
+              .style15, // Choose the nav bar style with this property.
         ),
-        popAllScreensOnTapOfSelectedTab: true,
-        popActionScreens: PopActionScreensType.all,
-        itemAnimationProperties: const ItemAnimationProperties(
-          // Navigation Bar's items animation properties.
-          duration: Duration(milliseconds: 200),
-          curve: Curves.ease,
-        ),
-        screenTransitionAnimation: const ScreenTransitionAnimation(
-          // Screen transition animation on change of selected tab.
-          animateTabTransition: true,
-          curve: Curves.ease,
-          duration: Duration(milliseconds: 200),
-        ),
-        navBarStyle:
-            NavBarStyle.style15, // Choose the nav bar style with this property.
       ),
     );
   }
